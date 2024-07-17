4285e9f725da911699840e358211c83316f94c69
ArtemKorotich <korotich2004@inbox.ru>
2024-07-17 14:46:18 +0300

    models(jokes)

---
+++ collection_of_jokes/jokes/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Theme(models.Model):
+    name=models.CharField(max_length=100,unique=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class Content(models.Model):
+     CONTENT_TYPES=[
+         ('joke','Joke')
+         ('image','Image')
+     ]
+     content_types=models.CharField(max_length=10,choices=CONTENT_TYPES)
+     theme=models.ForeignKey(Theme,on_delete=models.CASCADE)
+     text=models.TextField(blank=True,null=True)
+     image=models.ImageField(upload_to='/images',blank=True,null=True)
+     rating=models.IntegerField(default=0)
+     
+     def __str__(self):
+         return self.text if self.content_types=='joke' else self.image.url
+     
+class Favorite(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    content=models.ForeignKey(Content,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.user.username}-{self.content}"
